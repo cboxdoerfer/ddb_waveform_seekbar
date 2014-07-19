e8278cf8c308b0e225e68969f5e7619d88d8818e
Cuithinien <alioshabp@gmail.com>
2014-07-19 13:22:01 +0300

    Fix grammar and make the warning more clear

--- userinstall.sh
+++ userinstall.sh
@@ -16,7 +16,7 @@ CHECK_PATHS="/usr/local/lib/deadbeef /usr/lib/deadbeef"
 for path in $CHECK_PATHS; do
 	if [ -d $path ]; then
 		if [ -f $path/ddb_misc_waveform_GTK2.so -o -f $path/ddb_misc_waveform_GTK3.so ]; then
-			echo "Warning: Some version of the waveform plugin is present in $path, you should remove it to avoid conflicts!"
+			echo "Warning: Some version of the waveform plugin is already present in $path. You should remove it to avoid conflicts!"
 		fi
 	fi
 done
